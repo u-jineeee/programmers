a54c1c126c943ffd7a2be22fa92326d443ef30bf
ujineeee <yujin11006@naver.com>
2023-03-07 09:20:28 +0900

    자릿수 더하기 해결

---
+++ src/main/java/ll/p120906/Main.java
@@ -0,0 +1,18 @@
+package ll.p120906;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
+
+class Solution {
+    public int solution(int n){
+        int answer = 0;
+        while(n != 0){
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
