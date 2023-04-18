fc7e805c8a90d26a8b782f5c11e19b61aa41d456
ujineeee <yujin11006@naver.com>
2023-04-18 15:17:06 +0900

    컨트롤 제트 해결

---
+++ src/test/java/ll/p120853/Tests.java
@@ -0,0 +1,14 @@
+package ll.p120853;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Tests {
+    @Test
+    @DisplayName("1 2 Z 3 => 4")
+    void test001(){
+        assertThat(Solution.solution("1 2 Z 3")).isEqualTo(4);
+    }
+}
