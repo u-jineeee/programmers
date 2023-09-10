0a087c2663d00e543928ee4d932a3fcd93513db6
ujineeee <yujin11006@naver.com>
2023-09-11 01:06:01 +0900

    직사각형 별찍기 문제 해결

---
+++ src/main/java/ll/level1/p12969/Main.java
@@ -0,0 +1,21 @@
+package ll.level1.p12969;
+
+import java.util.Scanner;
+
+public class Main {
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		for (int i = 0; i < b; i++) {
+			for (int j = 0; j < a; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
