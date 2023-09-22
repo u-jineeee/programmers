3c18efbfce03fe5d69bb9df6940b0bab4a707c60
ujineeee <yujin11006@naver.com>
2023-09-23 02:37:12 +0900

    기사단원의 무기 문제 해결

---
+++ src/main/java/ll/level1/p136798/Main.java
@@ -0,0 +1,25 @@
+package ll.level1.p136798;
+
+public class Main {
+}
+
+class Solution {
+	public int solution(int number, int limit, int power) {
+		int answer = 0;
+
+		for (int i = 1; i <= number; i++) {
+			int cnt = 0;
+			for (int j = 1; j <= Math.sqrt(i); j++) {
+				if (i % j == 0) {
+					cnt += 2;
+					if (i / j == j)
+						cnt--;
+				}
+			}
+
+			if (cnt > limit) answer += power;
+			else answer += cnt;
+		}
+		return answer;
+	}
+}
