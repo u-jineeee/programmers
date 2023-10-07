2548d29a42e0cec1a088dfdaf819925a7d1d1574
ujineeee <yujin11006@naver.com>
2023-10-08 03:49:10 +0900

    숫자의 표현 문제 해결

---
+++ src/test/java/ll/level2/p12924/Tests.java
@@ -0,0 +1,14 @@
+package ll.level2.p12924;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+	@Test
+	@DisplayName("예제1")
+	void test001() {
+		assertThat(new Solution().solution(15)).isEqualTo(4);
+	}
+}
